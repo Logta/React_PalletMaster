51e39397f8360587b2925759ab73c1ba9a724fd8
logta <takenobu15@gmail.com>
2020-02-20 00:00:37 +0900

    build

---
+++ palletmaster/build/precache-manifest.c683fc0500445498f50ecacd552e9db3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383f4a9bf7f488e2c9568462aad33b9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7650eee81b9d3fecdc75",
+    "url": "/static/css/main.dc155aa7.chunk.css"
+  },
+  {
+    "revision": "806e42e5271ca565b2ee",
+    "url": "/static/js/2.11c72633.chunk.js"
+  },
+  {
+    "revision": "48a7f982264b141da44db572a26ecb94",
+    "url": "/static/js/2.11c72633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7650eee81b9d3fecdc75",
+    "url": "/static/js/main.9df930cf.chunk.js"
+  },
+  {
+    "revision": "8601ab1414eecad71d09",
+    "url": "/static/js/runtime-main.950162e1.js"
+  },
+  {
+    "revision": "02fe71ee2c9a19a7dde8deb3cb126ae6",
+    "url": "/static/media/PM_Desine.02fe71ee.png"
+  }
+]);
