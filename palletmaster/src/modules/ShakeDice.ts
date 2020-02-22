8690e6204c9045b7915a3743cce851e62e4e7afe
logta <takenobu15@gmail.com>
2020-02-23 01:16:15 +0900

    fix #14

--- palletmaster/src/modules/ShakeDice.ts
+++ palletmaster/src/modules/ShakeDice.ts
@@ -10,26 +10,34 @@ interface Result {
     result: string;
 }
 
-function ShakeDice(item: Item): Result {
+const ShakeDice = (item: Item): Result => {
     let result: Result = {
         ok: '',
         result: '',
     };
 
-    let _1d100Value: number = Math.round(Math.random() * 100) + 1;
+    let _1d100Value: number = Math.floor(Math.random() * 100) + 1;
 
-    if (_1d100Value <= 5) {
-        result.ok = 'クリティカル';
-    } else if (_1d100Value <= +item.diceValue) {
-        result.ok = '成功';
-    } else {
-        result.ok = '失敗';
-    }
+    result.ok = getOkStatus(_1d100Value, +item.diceValue);
 
     result.result =
         '技能:' + item.name + ' ' + _1d100Value + ' <= ' + item.diceValue;
 
     return result;
-}
+};
+
+export const getOkStatus = (diceValue: number, skillValue: number): string => {
+    if (diceValue <= 5) {
+        return 'クリティカル';
+    } else if (diceValue <= skillValue) {
+        return '成功';
+    } else {
+        return '失敗';
+    }
+};
+
+export const ShakeOneDice = (num: number): number => {
+    return Math.floor(Math.random() * num) + 1;
+};
 
 export default ShakeDice;
--- palletmaster/src/tests/modules/shakeNDNDice.test.js
+++ palletmaster/src/tests/modules/shakeNDNDice.test.js
@@ -1,4 +1,5 @@
 import shakeNDNDice, { IntSumToString } from '../../modules/shakeNDNDice';
+
 describe('shakeNDNDice', () => {
     let ItemNDN;
     beforeAll(() => {
@@ -19,11 +20,23 @@ describe('shakeNDNDice', () => {
             expect(counter.ok).toBe('1D2');
             expect(counter.result).toMatch(/[0-9]{1}/);
         });
+        test('Math.random関数の返答が0.5未満', () => {
+            const randomMock = jest.spyOn(Math, 'random');
+            randomMock.mockReturnValue(0.2);
+            const counter = shakeNDNDice(ItemNDN);
+            expect(counter.result).toBe('1');
+        });
+        test('Math.random関数の返答が0.5以上', () => {
+            const randomMock = jest.spyOn(Math, 'random');
+            randomMock.mockReturnValue(0.8);
+            const counter = shakeNDNDice(ItemNDN);
+            expect(counter.result).toBe('2');
+        });
     });
 
     describe('IntSumToString()', () => {
         test('sumToString', () => {
-            const numbers = [1, 2, 3]; // increment上記と同じ
+            const numbers = [1, 2, 3];
             const sum = IntSumToString(numbers);
             expect(sum).toBe('6');
         });
