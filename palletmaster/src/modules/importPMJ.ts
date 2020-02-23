233c720311a432af83b7189519fd5d9408df5062
logta <takenobu15@gmail.com>
2020-02-23 21:35:37 +0900

    fix #23

--- palletmaster/src/modules/importPMJ.ts
+++ palletmaster/src/modules/importPMJ.ts
@@ -1,9 +1,9 @@
-export default function onSubmit(func, item) {
+export default function onSubmit(func: any, item: any) {
     if (item == null) return;
     let reader = new FileReader();
 
     // jsonファイル,pmjファイル以外は処理を止める
-    if (!item.name.match('.json$') && !item.name.match('.pmj$')) {
+    if (checkFileTypeJSONorPMJ(item.name)) {
         alert('jsonファイル、またはpmjファイルを選択してください');
         return;
     }
@@ -12,14 +12,28 @@ export default function onSubmit(func, item) {
     reader.onerror = function() {
         alert('ファイル読み取りに失敗しました');
     };
+
     //読み込んだファイルの中身を取得する
     reader.readAsText(item, 'UTF-8');
 
     //ファイルの中身を取得後に処理を行う
     if (item != null) {
         reader.addEventListener('load', function() {
-            let json = JSON.parse(reader.result);
-            func(json);
+            if (
+                reader.result != null &&
+                !(reader.result instanceof ArrayBuffer)
+            ) {
+                let json = JSON.parse(reader.result);
+                func(json);
+            }
         });
     }
 }
+
+export const checkFileTypeJSONorPMJ = (fileName: string): boolean => {
+    // jsonファイル,pmjファイル以外は処理を止める
+    if (!fileName.match('.json$') && !fileName.match('.pmj$')) {
+        return false;
+    }
+    return true;
+};
